4cdeb445e0a6ab2aaf6df1d4ba332ca7e61fd000
harshverma250202 <harshverma250202@gmail.com>
2021-07-17 22:02:58 +0530

    final app

--- src/Dash.js
+++ src/Dash.js
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 
 export const Dash = () => {
- 
+
     const [data, setData] = useState([
         {
             "name": "My First Blog",
@@ -17,19 +17,26 @@ export const Dash = () => {
     let sub = (e) => {
         e.preventDefault();
         // console.log('hiii')
-        
+
         let obj = {};
         obj['name'] = document.getElementById('username').value;
         obj['text'] = document.getElementById('text').value;
-        obj['sn'] = data.length + 1
+        obj['sn'] = data[data.length-1].sn + 1
         obj['like'] = 0
         obj['dislike'] = 0
+
         setData([...data, obj]);
+        document.getElementById('modaltitle').innerHTML='Hello  '+obj.name+'!';
+        document.getElementById('modalbody').innerHTML='you have succesfully Shared Post with Post No. : '+obj.sn;
+        document.getElementById('imaginaryclick').click();
         console.log(data);
-        document.getElementById('postform').reset();
-        
+        document.getElementById('mainform').reset();
+
     }
     let del = (item) => {
+        document.getElementById('modaltitle').innerHTML='Hello  '+item.name+'!';
+        document.getElementById('modalbody').innerHTML='you have succesfully deleted Post with Post No. : '+item.sn;
+        document.getElementById('imaginaryclick').click();
 
         setData(data.filter((e) => { return e !== item }))
     }
@@ -45,10 +52,13 @@ export const Dash = () => {
         btn1.style.display = 'none'
         let textid = 'text' + item.sn
         let selecttext = document.getElementById(textid)
-        selecttext.disabled=false
+        selecttext.disabled = false
 
     }
     let saveedit = (item) => {
+        document.getElementById('modaltitle').innerHTML='Hello  '+item.name+'!';
+        document.getElementById('modalbody').innerHTML='you have succesfully Edited Post with Post No. : '+item.sn;
+        document.getElementById('imaginaryclick').click();
         let id = 'savebtn' + item.sn;
         let btn = document.getElementById(id);
         btn.style.display = 'none';
@@ -57,7 +67,7 @@ export const Dash = () => {
         btn1.style.display = 'inline-block'
         let textid = 'text' + item.sn
         let selecttext = document.getElementById(textid)
-        selecttext.disabled=true
+        selecttext.disabled = true
     }
 
 
@@ -95,30 +105,30 @@ export const Dash = () => {
     return (
         <div className="container">
 
-           <center>
-            <form onSubmit={sub} className='mt-5 mb-5' id='postform'>
-                <h1>Create Post</h1>
-                <div className="mb-3">
-                    {/* <label for="username" className="form-label">Username</label> */}
-                    <input type="text" className="form-control" id="username"  placeholder="UserName" required/>
-                </div>
-                <div className="mb-3">
-                    {/* <label for="text" className="form-label">text</label><br /> */}
-                    <textarea name="text" className="form-control" id="text" placeholder="Message to Sent" required></textarea>
-                </div>
+            <center>
+                <form onSubmit={sub} id='mainform' className='mt-5 mb-5 postform' >
+                    <h1>Create Post</h1>
+                    <div className="mb-3">
+                        {/* <label for="username" className="form-label">Username</label> */}
+                        <input type="text" className="form-control" id="username" placeholder="UserName" required />
+                    </div>
+                    <div className="mb-3">
+                        {/* <label for="text" className="form-label">text</label><br /> */}
+                        <textarea name="text" className="form-control" id="text" placeholder="Message to Sent" required></textarea>
+                    </div>
 
-                <button type="submit" className="btn btn-primary">Share</button>
-            </form></center>
+                    <button type="submit" className="btn btn-primary">Share</button>
+                </form></center>
 
 
             <div className="posts ">{data.map((info) => {
-                return <center> <div className='mt-5 mb-5'>  <hr/><form id='postform'>
-                   <b><h2>Post : {info.sn}</h2></b> 
+                return <center> <div className='mt-5 mb-5'>  <hr /><form className='postform'>
+                    <b><h2>Post : {info.sn}</h2></b>
                     <div className="mb-3">
-                       <h3> Username : {info.name} </h3>
-                           
-                        </div>
-                        <div className="mb-3">
+                        <h3> Username : {info.name} </h3>
+
+                    </div>
+                    <div className="mb-3">
                         {/* <label for="text" className="form-label"><h3>text</h3></label><br /> */}
                         <textarea name="text" className="form-control" onChange={(e1) => {
                             setData(
@@ -129,7 +139,7 @@ export const Dash = () => {
                                 ))
                         }} id={"text" + info.sn} value={info.text} disabled></textarea>
                     </div>
-                    </form>
+                </form>
                     <button className="btn btn-success me-3" onClick={() => { like(info) }}>like : {info.like}</button>
                     <button className='btn btn-info me-3' onClick={() => { dislike(info) }} >dislike : {info.dislike}</button>
                     <br />
@@ -138,12 +148,40 @@ export const Dash = () => {
                     <button className="btn btn-danger me-3" onClick={() => { del(info) }} >delete</button>
                     <button className='btn btn-dark me-3' id={'editbtn' + info.sn} onClick={() => { edit(info) }}>Edit-text</button>
                     <button className='btn btn-primary me-3' style={style} id={'savebtn' + info.sn} onClick={() => { saveedit(info) }}>Save</button>
-                    
-                    <hr/></div></center>
-                   })} </div>
-                  
 
-        </div>    
+                    <hr /></div></center>
+            })} </div>
+
+
+
+
+
+            
+            <button type="button" className="btn btn-primary" style={style} data-bs-toggle="modal" id='imaginaryclick' data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+           
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="modaltitle"></h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body" id="modalbody">
+                        
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
 
 
 
